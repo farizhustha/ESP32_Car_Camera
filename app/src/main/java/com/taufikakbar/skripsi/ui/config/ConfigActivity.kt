e976c863b807c8770cf2cd24d6e74af63b117cc5
Fariz Hustha <farizhustha205@gmail.com>
2023-06-17 16:25:46 +0700

    Initialization Project & Setup Package

---
+++ app/src/main/java/com/taufikakbar/skripsi/ui/config/ConfigActivity.kt
@@ -0,0 +1,12 @@
+package com.taufikakbar.skripsi.ui.config
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.taufikakbar.skripsi.R
+
+class ConfigActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_config)
+    }
+}
